9f16e2d99eb24b940527f423650c85324bb96022
Sorawong L <Sorawong2543@gmail.com>
2023-11-24 18:11:31 +0700

    chore: add comment

--- src/types/express.ts
+++ src/types/express.ts
@@ -1,5 +1,7 @@
 import { Request } from "express"
 
+// i want req.body has type, but not sure how to set type via middleware, so i create custom type that receive generic type and prefer generic type as body
+// https://stackoverflow.com/a/55413670
 export interface ExpressCustomRequestBody<T> extends Request {
   body: T
 }
