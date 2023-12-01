c11afdfd6d6cbe01de6686d549daa420b3691305
Sorawong L <Sorawong2543@gmail.com>
2023-12-01 18:18:15 +0700

    fix: create post body request

--- src/modules/post/post.schema.ts
+++ src/modules/post/post.schema.ts
@@ -1,9 +1,7 @@
 import { z } from "zod"
 
 export const CreatePostBodyRequest = z.object({
-  title: z.string(),
   content: z.string(),
-  authorId: z.string(),
 })
 
 export type CreatePostBodyRequest = z.infer<typeof CreatePostBodyRequest>
